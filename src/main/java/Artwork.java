e9a01c373bf3d658a7aaeb1d069fb7bcdd30bf02
Kajanan <Kajanan15i@gmail.com>
2024-02-08 15:24:46 +0000

    Created Artwork Class

---
+++ src/main/java/Artwork.java
@@ -0,0 +1,12 @@
+public class Artwork {
+
+    private String title;
+    private String artist;
+    private double price;
+
+    public Artwork(String title, String artist, double price){
+        this.title = title;
+        this.price = price;
+        this.artist = artist;
+    }
+}
